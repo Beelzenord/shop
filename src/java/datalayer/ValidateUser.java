85788dd6a8b9d4e190131728697797d2261182e9
Beelzenord <fauzianordlund@192.168.8.101>
2017-10-01 21:07:31 +0200

    Merge origin/master
    Conflicts:
    	src/java/controller/ControllerApplication.java

--- src/java/datalayer/ValidateUser.java
+++ src/java/datalayer/ValidateUser.java
@@ -185,5 +185,85 @@ public class ValidateUser {
         
         
     }
+    public static void issueGrant(User u , Connection con){
+       // CREATE USER IF NOT EXISTS 'u1'@'localhost' identified BY 'u1';
+       //GRANT SELECT, INSERT, UPDATE ON shoeClass TO 'u1'@'localhost';
+       //GRANT SELECT, INSERT, UPDATE ON shirtClass TO 'u1'@'localhost';
+       //GRANT SELECT, INSERT, UPDATE ON glovesClass TO 'u1'@'localhost';
+       //GRANT SELECT, INSERT, UPDATE ON pantsClass TO 'u1'@'localhost';
+       //GRANT SELECT, INSERT, UPDATE ON user TO 'u1'@'localhost';
+       //GRANT SELECT, INSERT, UPDATE, DELETE ON orders TO 'u1'@'localhost';
+       //GRANT SELECT, INSERT, UPDATE ON orderDetails TO 'u1'@'localhost';
+    PreparedStatement ps1 = null;
+    PreparedStatement ps2 = null;
+    PreparedStatement ps3 = null;
+    PreparedStatement ps4 = null;
+    PreparedStatement ps5 = null;
+    PreparedStatement ps6 = null;
+    PreparedStatement ps7 = null;
+    PreparedStatement ps8 = null;
+    ResultSet rs = null;
+   
+     try {  
+         ps1 = con.prepareStatement("CREATE USER IF NOT EXISTS ? @'localhost' identified BY ? ");
+         ps2 = con.prepareStatement("GRANT SELECT, INSERT, UPDATE ON shoeClass TO ?@'localhost' ");
+         ps3 = con.prepareStatement("GRANT SELECT, INSERT, UPDATE ON shirtClass TO ?@'localhost' ");
+         ps4 = con.prepareStatement("GRANT SELECT, INSERT, UPDATE ON glovesClass TO ?@'localhost' ");
+         ps5 = con.prepareStatement("GRANT SELECT, INSERT, UPDATE ON pantsClass TO ?@'localhost' ");
+         ps6 = con.prepareStatement("GRANT SELECT, INSERT, UPDATE ON user TO ?@'localhost' ");
+         ps7 = con.prepareStatement("GRANT SELECT, INSERT, UPDATE, DELETE ON orders TO ?@'localhost' ");
+         ps8 = con.prepareStatement("GRANT SELECT, INSERT, UPDATE ON orderDetails TO '?'@'localhost' ");
+         ps1.setString(1, u.getUsername());
+         ps1.setString(2, u.getUsername());
+         ps2.setString(1, u.getUsername());
+         ps3.setString(1, u.getUsername());
+         ps4.setString(1, u.getUsername());
+         ps5.setString(1, u.getUsername());
+         ps6.setString(1, u.getUsername());
+         ps7.setString(1, u.getUsername());
+         ps8.setString(1, u.getUsername());
+         ps1.execute();
+         ps2.execute();
+         ps3.execute();
+         ps4.execute();
+         ps5.execute();
+         ps6.execute();
+         ps7.execute();
+         ps8.execute();
+        
+     } catch (SQLException ex) {
+                Logger.getLogger(ValidateUser.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    
+    
+    }
+    public static String returnUserName(Connection con, String username){
+        PreparedStatement ps= null;
+        ResultSet rs = null;
+        String proof=null;
+         try {
+            
+            ps = con.prepareStatement("select User from mysql.user where user = ?");
+            ps.setString(1, username);
+            rs = ps.executeQuery();
+            if(rs.next()){ 		// id                    username             pass                     fn                       ln                      
+    		proof = rs.getString("User");
+            }
+            return proof;
+            /*
+            
+            if(username.equals(proof)){
+                return "checked";
+            }
+            else{
+                return "false";
+            }*/
+        } catch (SQLException ex) {
+            Logger.getLogger(ValidateUser.class.getName()).log(Level.SEVERE, null, ex);
+            //con.close();
+            return null;
+        }
+      
+    }
     
 }
